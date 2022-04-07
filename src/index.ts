b3d5d64825ee78246ac2547337af180fe7c68f94
v8solucoes <solucoesv8@gmail.com>
2022-04-07 16:50:00 -0300

    novoBackand

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import * as cors from "cors";
-
+import { deletar, editar, pegar, listar, novo, usuario } from './servico-credenciais/firebase'
 import { ModeloRequisicao } from "./index-modulo";
 import { Requisicao } from "../../construtor/src/construtor/requisicao/requisicao.interface";
-
+import { Request, Response } from 'express';
+import { acao } from './servico-credenciais/variaveis'
 var credenciais = express();
 
 credenciais.use(cors());
@@ -24,46 +25,17 @@ credenciais.get("/", async (req: express.Request, res: express.Response) => {
   });
 });
 
-credenciais.get("/criar-dados",
+credenciais.post("/cadastrar",
 
   async (req: express.Request, res: express.Response) => {
  
-  /*   const requisicao: Requisicao = {
-      credencial: dados_Dados.usuarioAdm.credencial,
-      dados: dados_Dados
-    } */
-
-    
-
-    try {
-   /*    const resposta = await new ModeloRequisicao().crud(requisicao)
-      res.json(resposta); */
-/*       req.headers.host */
-
-      res.json({
-        'origem':` ${req.headers.host}`,
-      });
-   
-    } catch (error) {
-      res.status(500).render("index", {
-        title: "Erro do Servidor",
-        message: error,
-      });
-    }
-  }
-);
-
-credenciais.post(
-  "/credenciais",
-  async (req: express.Request, res: express.Response) => {
-   
-    /* const requisicao: Requisicao = req.body; */
-    const requisicao: Requisicao = req.body
+    const requisicao = req.body as Requisicao
 
     try {
       const resposta = await new ModeloRequisicao().crud(requisicao)
+  
       res.json(resposta);
-   
+      
     } catch (error) {
       res.status(500).render("index", {
         title: "Erro do Servidor",
@@ -72,6 +44,34 @@ credenciais.post(
     }
   }
 );
+
+credenciais.post('/credenciais', async (req: Request, res: Response) => {
+
+  const dadosCliente = req.body as any
+  console.log(dadosCliente)
+
+  switch (dadosCliente.acao) {
+
+    case  acao.usuario : res.send(await usuario('usuario', dadosCliente.chave)); break;
+
+    case acao.novo  : res.send(await novo(dadosCliente)); break;
+
+    case acao.editar: res.send(await editar(dadosCliente)); break;
+
+    case acao.pegar : res.send(await pegar(dadosCliente)); break;
+
+    case acao.listar: res.send(await listar(dadosCliente)); break;
+
+    case acao.deletar: res.send(await deletar(dadosCliente)); break;
+
+    default:
+      const erro = `Ação não encontrada: ${dadosCliente.acao}`
+      console.log(erro)
+      res.status(200).send(erro)
+      break;
+  };
+
+})
 
 credenciais.use(function (req, res, next) {
   res.status(404).send("Desculpe Url não encontrada!");
--- src/servico-criar-modulos/modulo-nomes.ts
+++
@@ -1,7 +0,0 @@
-import { ModuloIdUrl } from "../../../_interface/interfaces-imports";
-
-export const moduloUsuarioAdm: ModuloIdUrl = {
-    nome: 'Usuarios',
-    id: 'usuarioAdm',
-    url: 'usuario-adm',
-}
