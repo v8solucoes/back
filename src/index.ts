2d054f1449e454f644e8820afff3e60d4fde0dd8
v8solucoes <solucoesv8@gmail.com>
2022-08-23 05:41:41 -0300

    test request

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import * as cors from "cors";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { deletar, editar, pegar, listar, novo, usuario } from './servico-credenciais/firebase'
-import { ModeloRequisicao } from "./index-modulo";
-import { Request, Response } from 'express';
-import { acao } from './servico-credenciais/variaveis'
-import { Funcoes } from '../../construtor/src/funcoes/back/funcoes'
-import { Requisicao, Resposta } from "../../construtor/src/construtor/interface/interface";
-=======
-import { ValidatorsBack } from "../../domain/src/shared/validator-back";
-import { FunctionsBack } from "../../domain/src/shared/functions-back";
-import { Irequest, IValidatorRequest } from "@domain/interface";
-
->>>>>>> 4e243a23b8ddfd0e9639b13bd5f48bf98cd00d5b
-=======
 import { ValidatorsRemote } from "../../domain/src/shared/validator-remote";
 import { TestCompose } from "../../domain/src/shared/validator-local";
 import { Module } from "../../domain/src/shared/modules";
 import { Irequest } from "@domain/interface";
->>>>>>> c1975bb74658e1fc7c0002ed411f752bf7f47228
 
 var credenciais = express();
 
